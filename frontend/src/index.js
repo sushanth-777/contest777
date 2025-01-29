278aeff52601c0d2d1e617fcf4cfbd432202201b
Sushanth Reddy <63182782+sushanth-777@users.noreply.github.com>
2025-01-29 15:07:26 -0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
